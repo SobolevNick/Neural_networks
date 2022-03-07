d2621ac326fb1a4ea5ee5a8f82601465f57bf471
SobolevNick <93708986+SobolevNick@users.noreply.github.com>
2022-03-07 13:23:53 +0300

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,81 +1,88 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <stdlib.h>
-class sigma { //класс реализует две функции: сигмоиду и производную сигмоиды
+#include <Eigen/Dense>
+
+class sigma {
 public:
-    float evaluate0(float x) { //сигмоида
+    float evaluate0(float x) {
         return 1/(1+exp(-x));
     }
-    float evaluate1(float x) { //производная сигмоиды
+    float evaluate1(float x) {
         return exp(x)/pow((1+exp(x)),2);
     }
 };
-class building_block { //класс, в котором генерируется матрица A и вектор b 
-public: //случайных чисел, а также функция, строящая по данному вектору x, 
-    int n = 0; //A, b и sigma() предсказание, и в заключении градиента A, b и x.  
-    int m = 0;
-    std::vector <float> x = {};
-    std::vector <std::vector<float>> A;
-    std::vector <float> b = {};
-    std::vector <float> u = {};
-    building_block(std::vector <float> y, std::vector <float> z) {//генерирует A и b
-        x = y;
-        u = z;
-        n = y.size();
-        m = z.size();
-        A = std::vector<std::vector<float>> (n, std::vector<float>(m, 0)) ;
-        b = std::vector <float> (m, 0);
-        for (int k = 0; k < m; k++) {
-            for (int j = 0; j < n; j++) {
-                A[k][j] = -1 + rand() % 3; //я пока что зарандомил из трёх чисел 
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            b[i] = -1 + rand() % 3;
-        }
-    }
-    std::vector <float> theta() {//вычисляет Ax+b
-        std::vector <float> f(m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++){
-                f[i] = f[i] + A[i][j] * x[j];
-            }
-            f[i] = f[i] + b[i];
-        }
-        return f;
-    }
-    std::vector <float> prediction() {//функция предсказания
-        sigma sigma0;
-        std::vector <float> f = theta();
-
-        for (int i = 0; i < m; i++) {
-            f[i] = sigma0.evaluate0(f[i]);
-        }
-        return f;
-    }
-    std::vector <std::vector<float>> sigma_der() {//вычисляет диагональную матрицу, 
-        std::vector <float> f = theta();//на диагонали которой стоят производные sigma()
-        std::vector <std::vector<float>> sd(m, std::vector<float>(m,0));//от Ax+b
-        sigma sigma1;
-        for (int i = 0; i < m; i++) {
-            sd[i][i] = sigma1.evaluate1(f[i]);
-        }
-        return sd;
-    };
-    std::vector <float> grad_b() {//градиент b
-        std::vector <std::vector<float>> f = sigma_der();
-        sigma sigma1;
-        std::vector <float> gb(m,0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-               gb[i] = gb[i] + f[i][j]*u[j];
-            }
-        }
-        return gb;
+class theta {
+    public:
+    Eigen::VectorXf count_theta(Eigen::MatrixXf A, Eigen::VectorXf b, Eigen::VectorXf x) {
+        return A*x + b;
     }
 };
-
+class building_block {
+private:
+    Eigen::MatrixXf A_;
+    Eigen::VectorXf b_;
+public:
+    building_block(int m, int n) {
+        A_ = Eigen::MatrixXf::Random(m,n);
+        b_ = Eigen::VectorXf::Random(m);
+        }
+    void check_mat() {
+        std::cout << A_ << std::endl;
+    };
+    void check_vec () {
+        std::cout << b_ << std::endl;
+    };
+    Eigen::VectorXf predict(Eigen::VectorXf x) {
+        sigma s;
+        theta t;
+        int m = t.count_theta(A_,b_,x).size();
+        Eigen::VectorXf pred(m);
+        for (int i = 0; i < m; i++)  {
+            pred(i) = s.evaluate0(t.count_theta(A_,b_,x)(i));
+        }
+        return pred;
+    }
+    Eigen::MatrixXf count_grad_A(Eigen::VectorXf x, Eigen::RowVectorXf u) {
+        theta t;
+        sigma s;
+        int m = u.size();
+        Eigen::MatrixXf S(m,m);
+        for (int i = 0; i < m; i++) {
+            S(i,i) = s.evaluate1(t.count_theta(A_,b_,x)(i));
+        }
+        return S*u.transpose()*x.transpose();
+    }
+    Eigen::VectorXf count_grad_b(Eigen::VectorXf x, Eigen::RowVectorXf u) {
+        theta t;
+        sigma s;
+        int m = u.size();
+        Eigen::MatrixXf S(m,m);
+        for (int i = 0; i < m; i++) {
+            S(i,i) = s.evaluate1(t.count_theta(A_,b_,x)(i));
+        }
+        return S*u.transpose();
+    }
+    Eigen::RowVectorXf count_grad_x(Eigen::VectorXf x, Eigen::RowVectorXf u) {
+        theta t;
+        sigma s;
+        int m = u.size();
+        Eigen::MatrixXf S(m,m);
+        for (int i = 0; i < m; i++) {
+            S(i,i) = s.evaluate1(t.count_theta(A_,b_,x)(i));
+        }
+        return u*S*A_;
+    }
+};
 int main() {
+    building_block bb(4,3);
+    bb.check_mat();
+    std::cout << std::endl;
+    bb.check_vec();
+    std::cout << std::endl;
+    Eigen::VectorXf x(3);
+    x << 1,0,1;
+    Eigen::RowVectorXf u(4);
+    u << -1,2,0,1;
+    std::cout << bb.count_grad_x(x,u);
     return 0;
 }
