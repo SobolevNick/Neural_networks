2a5f3182e1a544c8697ef9639cfacbfa3c9d7d77
SobolevNick <93708986+SobolevNick@users.noreply.github.com>
2022-03-11 15:43:20 +0300

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,81 +1,90 @@
-#include <iostream>
+#include <Eigen/Dense>
 #include <cmath>
-#include <vector>
-#include <stdlib.h>
-class sigma { //класс реализует две функции: сигмоиду и производную сигмоиды
-public:
-    float evaluate0(float x) { //сигмоида
-        return 1/(1+exp(-x));
-    }
-    float evaluate1(float x) { //производная сигмоиды
-        return exp(x)/pow((1+exp(x)),2);
-    }
-};
-class building_block { //класс, в котором генерируется матрица A и вектор b 
-public: //случайных чисел, а также функция, строящая по данному вектору x, 
-    int n = 0; //A, b и sigma() предсказание, и в заключении градиента A, b и x.  
-    int m = 0;
-    std::vector <float> x = {};
-    std::vector <std::vector<float>> A;
-    std::vector <float> b = {};
-    std::vector <float> u = {};
-    building_block(std::vector <float> y, std::vector <float> z) {//генерирует A и b
-        x = y;
-        u = z;
-        n = y.size();
-        m = z.size();
-        A = std::vector<std::vector<float>> (n, std::vector<float>(m, 0)) ;
-        b = std::vector <float> (m, 0);
-        for (int k = 0; k < m; k++) {
-            for (int j = 0; j < n; j++) {
-                A[k][j] = -1 + rand() % 3; //я пока что зарандомил из трёх чисел 
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            b[i] = -1 + rand() % 3;
-        }
-    }
-    std::vector <float> theta() {//вычисляет Ax+b
-        std::vector <float> f(m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++){
-                f[i] = f[i] + A[i][j] * x[j];
-            }
-            f[i] = f[i] + b[i];
-        }
-        return f;
-    }
-    std::vector <float> prediction() {//функция предсказания
-        sigma sigma0;
-        std::vector <float> f = theta();
+#include <iostream>
 
-        for (int i = 0; i < m; i++) {
-            f[i] = sigma0.evaluate0(f[i]);
+namespace ML {
+    class sigma {
+    public:
+        using Matrix = Eigen::MatrixXf;
+        using Vector = Eigen::VectorXf;
+        static float evaluate0(float x) {
+            return 1 / (1 + exp(-x));
         }
-        return f;
-    }
-    std::vector <std::vector<float>> sigma_der() {//вычисляет диагональную матрицу, 
-        std::vector <float> f = theta();//на диагонали которой стоят производные sigma()
-        std::vector <std::vector<float>> sd(m, std::vector<float>(m,0));//от Ax+b
-        sigma sigma1;
-        for (int i = 0; i < m; i++) {
-            sd[i][i] = sigma1.evaluate1(f[i]);
-        }
-        return sd;
+        static float evaluate1(float x) {
+            return exp(x) / pow((1 + exp(x)), 2);
+        };
+        static Vector evaluate0(const Vector &x) {
+            return x.array().exp() / (1 + x.array().exp());
+        };
+        static Matrix evaluate1(const Vector &x) {
+            return (x.array().exp() / pow(1 + x.array().exp(), 2)).matrix().asDiagonal();
+        };
     };
-    std::vector <float> grad_b() {//градиент b
-        std::vector <std::vector<float>> f = sigma_der();
-        sigma sigma1;
-        std::vector <float> gb(m,0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-               gb[i] = gb[i] + f[i][j]*u[j];
-            }
-        }
-        return gb;
-    }
-};
 
+    class NetLayer {
+    public:
+        using Matrix = Eigen::MatrixXf;
+        using Vector = Eigen::VectorXf;
+        using RowVector = Eigen::RowVectorXf;
+        NetLayer(int m, int n)
+            : A_(Matrix::Random(m, n)), b_(Vector::Random(m)) {
+        }
+        void print_weights() const {
+            std::cout << A_ << std::endl;
+        };
+        void print_bias() const {
+            std::cout << b_ << std::endl;
+        };
+        Vector predict(const Vector &x) const {
+            return sigma::evaluate0(A_ * x + b_);
+        }
+        Matrix count_grad_A(Vector x, Vector u) const {
+            return sigma::evaluate1(A_ * x + b_) * u * x.transpose();
+        }
+        Vector count_grad_b(Vector x, Vector u) const {
+            return sigma::evaluate1(A_ * x + b_) * u;
+        }
+        RowVector count_grad_x(Vector x, Vector u) const {
+            return u.transpose() * sigma::evaluate1(A_ * x + b_) * A_;
+        }
+
+    private:
+        int in_size() const {//Не использовалась пока
+            return A_.cols();
+        }
+        int out_size() const {
+            return A_.rows();
+        }
+        Matrix A_;
+        Vector b_;
+    };
+}// namespace ML
+void test_net_layer() {
+    ML::NetLayer Layer1(30, 20);
+    Layer1.print_weights();
+    std::cout << std::endl;
+    ML::NetLayer Layer2(25, 35);
+    Layer2.print_bias();
+    std::cout << std::endl;
+}
+void test_back_propagation() {
+    ML::NetLayer Layer1(3, 2);
+    Eigen::VectorXf x(2);
+    x << -2, 3;
+    Eigen::VectorXf u(3);
+    u << -1, 2, 1;
+    std::cout << Layer1.predict(x) << std::endl
+              << std::endl;
+    std::cout << Layer1.count_grad_A(x, u) << std::endl
+              << std::endl;
+    std::cout << Layer1.count_grad_b(x, u) << std::endl
+              << std::endl;
+    std::cout << Layer1.count_grad_x(x, u) << std::endl;
+}
+void test_all() {
+    test_net_layer();
+    test_back_propagation();
+}
 int main() {
-    return 0;
+    test_all();
 }
