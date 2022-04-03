571b595432041afa200360ee0bb210030800e9f2
SobolevNick <93708986+SobolevNick@users.noreply.github.com>
2022-04-03 19:55:56 +0300

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,9 +1,12 @@
+#include <Eigen/Core>
 #include <Eigen/Dense>
+#include <EigenRand/EigenRand>
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 namespace ML {
-    class sigma {
+    class Sigma {
     public:
         using Matrix = Eigen::MatrixXf;
         using Vector = Eigen::VectorXf;
@@ -25,39 +28,74 @@ namespace ML {
     public:
         using Matrix = Eigen::MatrixXf;
         using Vector = Eigen::VectorXf;
+        Eigen::Rand::Vmt19937_64 urng{42};
+        Eigen::Rand::NormalGen<float> norm_gen{0, 10};
         NetLayer(int m, int n)
-            : A_(Matrix::Random(m, n)), b_(Vector::Random(m)) {
+            : A_(norm_gen.template generate<Matrix>(m, n, urng)), b_(norm_gen.template generate<Matrix>(m, 1, urng)) {
         }
         void print_weights() const {
             std::cout << A_ << std::endl;
-        };
+        }
         void print_bias() const {
             std::cout << b_ << std::endl;
-        };
+        }
         Vector predict(const Vector &x) const {
-            return sigma::evaluate0(A_ * x + b_);
+            return Sigma::evaluate0(A_ * x + b_);
         }
-        Matrix count_grad_A(Vector x, Vector u) const {
-            return sigma::evaluate1(A_ * x + b_) * u * x.transpose();
+        Matrix count_grad_A(const Vector &x, const Vector &u) const {
+            return Sigma::evaluate1(A_ * x + b_) * u * x.transpose();
         }
-        Vector count_grad_b(Vector x, Vector u) const {
-            return sigma::evaluate1(A_ * x + b_) * u;
+        Vector count_grad_b(const Vector &x, const Vector &u) const {
+            return Sigma::evaluate1(A_ * x + b_) * u;
         }
-        Vector count_grad_x(Vector x, Vector u) const {
-            return (u.transpose() * sigma::evaluate1(A_ * x + b_) * A_).transpose();
+        Vector count_grad_x(const Vector &x, const Vector &u) const {
+            return (u.transpose() * Sigma::evaluate1(A_ * x + b_) * A_).transpose();
         }
 
     private:
-        int in_size() const {//Не использовалась пока
-            return A_.cols();
-        }
-        int out_size() const {
-            return A_.rows();
-        }
         Matrix A_;
         Vector b_;
     };
+
+    class LossFunction {
+    public:
+        using Vector = Eigen::VectorXf;
+        static float count_loss_function(std::vector<std::pair<Eigen::VectorXf, Eigen::VectorXf>> x) {
+            NetLayer Layer(x[0].first.size(), x[0].second.size());
+            float sum = 0;
+            for (int i = 0; i < x.size(); i++) {
+                sum = (Layer.predict(x[i].first) - x[i].second).dot(Layer.predict(x[i].first) - x[i].second) + sum;
+            }
+            return sum / x.size();
+        }
+        static Vector count_starting_gradient(std::vector<std::pair<Eigen::VectorXf, Eigen::VectorXf>> x) {
+            NetLayer Layer(x[0].first.size(), x[0].second.size());
+            Vector sum(x[0].second.size());
+            sum = Vector::Zero(x[0].second.size());
+            for (int i = 0; i < x.size(); i++) {
+                sum = Layer.predict(x[i].first) - x[i].second + sum;
+            }
+            return 2 * sum / x.size();
+        }
+    };
+
+    class NeuralNetwork {
+        using Matrix = Eigen::MatrixXf;
+        using Vector = Eigen::VectorXf;
+        NeuralNetwork(Vector x, Vector y) {
+            NetLayer Layer((y.rows()), (x.rows()));
+        }
+        static std::pair<Matrix, Vector> count_theta_2(Matrix A, Vector b) {
+
+            return std::pair<Matrix, Vector>(A, b);
+        };
+
+    private:
+        Vector z_1;
+    };
+
 }// namespace ML
+
 void test_net_layer() {
     ML::NetLayer Layer1(30, 20);
     Layer1.print_weights();
@@ -66,6 +104,7 @@ void test_net_layer() {
     Layer2.print_bias();
     std::cout << std::endl;
 }
+
 void test_back_propagation() {
     ML::NetLayer Layer1(3, 2);
     Eigen::VectorXf x(2);
@@ -78,13 +117,36 @@ void test_back_propagation() {
               << std::endl;
     std::cout << Layer1.count_grad_b(x, u) << std::endl
               << std::endl;
-    std::cout << Layer1.count_grad_x(x, u) << std::endl;
+    std::cout << Layer1.count_grad_x(x, u) << std::endl
+              << std::endl;
 }
+
+void test_loss_function() {
+    Eigen::VectorXf x(5);
+    x << -2, 3.5, 5, -4, 0;
+    Eigen::VectorXf y(4);
+    y << -1, 2, 1, -2.7;
+    ML::LossFunction LF;
+    //std::cout << LF.count_loss_function(x, y);
+    //std::cout << LF.count_starting_gradient_i(x, y);
+}
+
+void test_neural_network() {
+    Eigen::VectorXf x(5);
+    x << -2, 3.5, 5, -4, 0;
+    Eigen::VectorXf y(4);
+    y << -1, 2, 1, -2.7;
+    //ML::NeuralNetwork NN;
+    //std::cout << NN.count_theta_2(x, y);
+    //std::cout << NN.count_starting_gradient(x, y);
+}
+
 void test_all() {
     test_net_layer();
     test_back_propagation();
+    test_loss_function();
 }
+
 int main() {
     test_all();
 }
-
